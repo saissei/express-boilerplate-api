dadf8958f69ed4a618eda4681a40084b74d43778
saissei <tariraritarara.214@gmil.com>
2019-07-14 21:17:46 +0900

    templateファイル配置完了

---
+++ src/routes/api.ts
@@ -0,0 +1,10 @@
+import * as express from 'express'
+import { Request, Response, NextFunction } from 'express'
+const router: express.Router = express.Router()
+
+/* GET home page. */
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.render('index', { title: 'Express' })
+})
+
+export = router
