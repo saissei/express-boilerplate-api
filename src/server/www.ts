51e754c95949fb8351e3988aaf7c0d10daadd081
issei.kariya <issei.kariya@zerobillbank.com>
2020-03-27 15:55:34 +0900

    必須パッケージ不足の解消、importファイルのtypo修正、ts用設定修正

--- src/server/www.ts
+++ src/server/www.ts
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-import app from './App';
+import app from './app';
 import Debug from 'debug';
 import logger from '../logger/LoggerBase';
 const debug: Debug.Debugger = Debug('express-debug');
